6acd795ea696abdeda8e55dbaf13fb2031591b79
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-12-16 23:33:32 +0200

    rm: remove colors.py file

--- ui/colors.py
+++
@@ -1,8 +0,0 @@
-"""Color selector utilities"""
-
-from config import *
-
-
-def draw_color_selector(screen, x, y, colors, current_color):
-    """Draw a color selector grid"""
-    pass  # Optional enhancement for future use
