938d3ab3ad2714708941a153a7aa4caf1eb3e682
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2025-12-16 23:35:23 +0200

    update: remove import from .colors file

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,6 +1,5 @@
 """UI module - User Interface components"""
 
 from .panel import draw_input_panel, handle_panel_click, init_input_fields
-from .colors import draw_color_selector
 
-__all__ = ['draw_input_panel', 'handle_panel_click', 'init_input_fields', 'draw_color_selector']
+__all__ = ['draw_input_panel', 'handle_panel_click', 'init_input_fields']
